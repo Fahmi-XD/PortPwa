019ba5204378d58f91a378ea79f618a2b803d5a1
Fahmi-XD <fahmixd404@gmail.com>
2024-01-27 06:31:00 +0700

    Update SW

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,72 @@
-// service-worker.js
+// The version of the cache.
+const VERSION = "v2";
 
-importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/6.2.4/workbox-sw.js"
-);
+// The name of the cache
+const CACHE_NAME = `period-tracker-${VERSION}`;
 
-workbox.setConfig({ debug: true });
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+    "/",
+    "/index.html",
+    "/js/main.js",
+    "/js/navbar.js",
+    "/js/home.js",
+    "/js/gsap.js",
+    "/css/all.css",
+    "/css/home.css",
+    "/css/navbar.css",
+    "/images/hero.svg",
+    "/images/logo.jpg",
+    "/offline.html"
+];
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
-
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || [], {
-    ignoreURLParametersMatching: [/.*/],
-    directoryIndex: null
+// On install, cache the static resources
+self.addEventListener("install", event => {
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll(APP_STATIC_RESOURCES);
+        })()
+    );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return (
-                response ||
-                fetch(e.request).catch(() => caches.match("/offline.html"))
+// delete old caches on activate
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map(name => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
             );
-        })
+            await clients.claim();
+        })()
+    );
+});
+
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", event => {
+    // As a single page app, direct app to always go to cached home page.
+    if (event.request.mode === "navigate") {
+        event.respondWith(caches.match("/"));
+        return;
+    }
+
+    // For all other requests, go to the cache first, and then the network.
+    event.respondWith(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(event.request.url);
+            if (cachedResponse) {
+                // Return the cached response if it's available.
+                return cachedResponse;
+            }
+            // If resource isn't in the cache, return a 404.
+            return new Response(null, { status: 404 });
+        })()
     );
 });
