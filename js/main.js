87bc5e6eea218e3964c76f5df591c1ac81bba9e7
Fahmi-XD <fahmixd404@gmail.com>
2024-01-28 06:50:13 +0700

    Update TWA

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    window.addEventListener("beforeunload", function (event) {
+document.addEventListener('DOMContentLoaded', () => {
+    const initialLink = window.location.href;
+
+    window.addEventListener('beforeunload', function (event) {
         // Eksekusi logika Anda di sini sebelum halaman ditutup
-        alert("Apakah anda yakin?");
+        alert('Apakah anda yakin?');
 
         // Anda dapat mengembalikan pesan konfirmasi (opsional)
-        event.returnValue = "Pesan konfirmasi";
+        event.returnValue = 'Pesan konfirmasi';
     });
+/*
+    window.addEventListener('popstate', function (event) {
+        if (window.location.href === initialLink) {
+            alert('Keluar dari website');
+            window.open(window.location, '_self').close();
+        }
+    });
+    */
 });
